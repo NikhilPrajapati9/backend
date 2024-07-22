fe5d2798b3003cb078fe821cb665b2c77720c64b
Nikhil Prajapati <nikhilprajapati3003@gmail.com>
2024-07-22 06:36:52 +0530

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/hello', (req, res) => {
+  res.send('Hello backend')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1>login not avilable now</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
